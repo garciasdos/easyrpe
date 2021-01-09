ef1722807db4ecf77fa72083f1259fa30158383f
Diego García <diegogarcia@MacBook-Pro.local>
2021-01-09 21:08:27 +0100

    Add some things

--- components/Navbar.js
+++ components/Navbar.js
@@ -3,63 +3,7 @@ import Head from 'next/head';
 import { Flex, Icon, IconButton, Link, Stack } from "@chakra-ui/react";
 import { SettingsIcon, QuestionIcon, createIcon } from '@chakra-ui/icons';
 import { useAuth } from '@/lib/auth';
-
-// using `path`
-export const Logo = createIcon({
-  displayName: "logo",
-  width:"500",
-   height:"316",
-   viewBox:"0 0 375 237",
-  // path can also be an array of elements, if you have multiple paths, lines, shapes, etc.
-  path: (
-    <>
-  <defs>
-    <symbol overflow="visible" id="a">
-      <path d="M36.2-41.887h.171L64.297 0H97.05L63.434-43.094c14.824-3.62 26.718-16.722 26.718-36.543 0-22.238-16.89-37.582-35.851-37.582H10.344V0h25.855zm13.788-54.988c7.586 0 15.516 6.55 15.516 17.238 0 11.203-7.93 17.41-15.516 17.41H36.2v-34.648zm0 0"/>
-    </symbol>
-    <symbol overflow="visible" id="b">
-      <path d="M36.2-41.887h18.1c18.962 0 35.852-13.965 35.852-37.75 0-22.238-16.89-37.582-35.851-37.582H10.344V0h25.855zm13.788-54.988c7.586 0 15.516 6.55 15.516 17.238 0 11.203-7.93 17.41-15.516 17.41H36.2v-34.648zm0 0"/>
-    </symbol>
-    <symbol overflow="visible" id="c">
-      <path d="M78.258-23.445H36.199V-47.06h40.336v-23.445H36.2v-23.27h42.059v-23.445H10.344V0h67.914zm0 0"/>
-    </symbol>
-    <symbol overflow="visible" id="d">
-      <path d="M22.93-6.871H10.605v-6.918h11.82v-6.871h-11.82v-6.817H22.93v-6.87H3.03V0H22.93zm0 0"/>
-    </symbol>
-    <symbol overflow="visible" id="e">
-      <path d="M21.719-13.133H13.03l4.344-12.222zM34.547 0L20.609-34.348h-6.418L.254 0h8.129l2.476-6.918h13.082L26.418 0zm0 0"/>
-    </symbol>
-    <symbol overflow="visible" id="f">
-      <path d="M16.363-5.355c-3.734 0-6.968-1.868-7.172-5.957H1.516C1.566-2.93 8.484.807 16.062.807c7.02 0 14.243-2.98 14.243-11.062 0-13.082-19.848-8.687-19.848-14.797 0-2.578 2.977-3.59 5.102-3.59 3.785 0 5.855 1.164 6.261 4.75h7.325c-.254-7.828-6.618-10.96-13.688-10.96-6.266 0-12.832 3.331-12.832 10.406 0 12.675 19.852 8.383 19.852 15.05 0 3.082-3.739 4.04-6.114 4.04zm0 0"/>
-    </symbol>
-    <symbol overflow="visible" id="g">
-      <path d="M13.082 0h7.578v-16.414l12.676-17.934h-9.04l-7.425 11.871-7.426-11.87H.402l12.68 17.933zm0 0"/>
-    </symbol>
-  </defs>
-  <use xlinkHref="#a" x="42.506" y="206.335" fill="red"/>
-  <use xlinkHref="#b" x="137.48" y="206.335" fill="red"/>
-  <use xlinkHref="#c" x="230.213" y="206.335" fill="red"/>
-  <use xlinkHref="#a" x="56.153" y="209.486" fill="#0ff"/>
-  <use xlinkHref="#b" x="151.127" y="209.486" fill="#0ff"/>
-  <use xlinkHref="#c" x="243.86" y="209.486" fill="#0ff"/>
-  <use xlinkHref="#a" x="49.33" y="207.91"/>
-  <use xlinkHref="#b" x="144.303" y="207.91"/>
-  <use xlinkHref="#c" x="237.037" y="207.91"/>
-  <use xlinkHref="#d" x="121.824" y="62.686" fill="red"/>
-  <use xlinkHref="#e" x="147.784" y="62.686" fill="red"/>
-  <use xlinkHref="#f" x="182.583" y="62.686" fill="red"/>
-  <use xlinkHref="#g" x="214.402" y="62.686" fill="red"/>
-  <use xlinkHref="#d" x="126.869" y="63.851" fill="#0ff"/>
-  <use xlinkHref="#e" x="152.829" y="63.851" fill="#0ff"/>
-  <use xlinkHref="#f" x="187.628" y="63.851" fill="#0ff"/>
-  <use xlinkHref="#g" x="219.446" y="63.851" fill="#0ff"/>
-  <use xlinkHref="#d" x="124.347" y="63.268"/>
-  <use xlinkHref="#e" x="150.307" y="63.268"/>
-  <use xlinkHref="#f" x="185.105" y="63.268"/>
-  <use xlinkHref="#g" x="216.924" y="63.268"/>
-  </>
-  ),
-})
+import { Logo } from './Logo';
 
 const Navbar = () => {     
   const auth = useAuth();
--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,52 @@
 import Head from 'next/head';
 import { useAuth } from '@/lib/auth';
-import { Flex, Button, ButtonGroup, Code, Heading, Text } from "@chakra-ui/react"
+import { Flex, Button, ButtonGroup, Code, Heading, Text, Box, createIcon } from "@chakra-ui/react"
+import { Logo } from '@/components/Logo';
+import { GoogleIcon } from '@/components/icons/GoogleIcon';
 
 const Home = () => {
   const auth = useAuth();
 
   return (
     <div>
-      <Head>
+            <Head>
         <title>Easy RPE</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Flex as="main" direction="column" align="center" justify="center">
-        <Heading>Easy RPE</Heading>
-        <Text>
-          Current user: <Code>{auth?.user ? auth.user.email : 'None'}</Code>
-        </Text>
-        {auth?.user ? (
-          <Button variant="ghost" mt={4} size="sm" onClick={(e) => auth.signout()}>Sign Out</Button>
-        ) : (
-          <Button variant="ghost" mt={4} size="sm" onClick={(e) => auth.signinWithGoogle()}>Sign In</Button>
-        )}
-      </Flex>
+      <Flex flexDirection="column" p={10}>
 
+        <Box
+          backgroundColor="white"
+          width="100%"
+          borderRadius="base"
+          display="flex"
+          flexDirection="column"
+          alignItems="center"
+          p={12}
+          boxShadow="lg"
+        >
+
+    <Logo fontSize="20vw"/>        
+
+    
+      <Button
+        onClick={() => auth.signinWithGoogle()}
+        backgroundColor="white"
+        color="gray.900"
+        variant="outline"
+        fontWeight="medium"
+        leftIcon={<GoogleIcon/>}
+        mt={4}
+        _hover={{ bg: 'gray.100' }}
+        _active={{
+          bg: 'gray.100',
+          transform: 'scale(0.95)'
+        }}
+      >
+        Continue with Google
+      </Button>
+      </Box>
+      </Flex>
       <footer>
       </footer>
     </div>
