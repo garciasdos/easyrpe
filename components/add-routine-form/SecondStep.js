8c30c92a05ce17298a8b0eed46a03f6189125cbb
Diego García <diegogarcia@MacBook-Pro.local>
2021-03-11 14:57:46 +0100

    Add Calendar Block

--- components/add-routine-form/SecondStep.js
+++ components/add-routine-form/SecondStep.js
@@ -1,28 +1,32 @@
 import { useAuth } from '@/lib/auth';
-import { Box, Flex, Heading } from '@chakra-ui/react';
+import { Box, Flex, Heading, Image, Text, VStack } from '@chakra-ui/react';
+import Calendar from 'model/value_objects/calendar';
+import Link from 'next/link';
+import Week from '../Calendar/Day';
 
 const SecondStep = (props) => {
-  const days = [];
-
   if (!props.routine) {
     return <></>;
   }
 
   console.log(props.routine);
 
-  let iteratorDate = props.routine.startDate;
-
-  for (
-    var day = props.routine.startDate;
-    day <= props.routine.endDate;
-    day.setDate(day.getDate() + 1)
-  ) {
-    days.push(<Box>{day.getDate()}</Box>);
-  }
-
+  const weeks = Calendar(props.routine.startDate, props.routine.endDate);
+  console.log(weeks);
   return (
-    <Flex flexDirection="column" p={10}>
-      {days}
+    <Flex>
+      <Flex flexDirection={'column'} spacing={4} width="300px">
+        {Object.values(weeks).map((week) => (
+          <Flex flexDirection={{ base: 'column', md: 'row' }}>
+            {Object.values(week).map((day) => (
+              <Box border="1px">
+                <Text>{day.getDate()}</Text>
+                {/* TODO: Each day component */}
+              </Box>
+            ))}
+          </Flex>
+        ))}
+      </Flex>
     </Flex>
   );
 };
---
+++ model/value_objects/calendar.js
@@ -0,0 +1,24 @@
+import { Flex } from '@chakra-ui/layout';
+
+Date.prototype.getWeekNumber = function () {
+  var d = new Date(
+    Date.UTC(this.getFullYear(), this.getMonth(), this.getDate())
+  );
+  d.setUTCDate(d.getUTCDate() - d.getUTCDay());
+  var yearStart = new Date(Date.UTC(d.getUTCFullYear(), 0, 1));
+  return Math.ceil(((d - yearStart) / 86400000 + 1) / 7);
+};
+
+function Calendar(startDate, endDate) {
+  const weeks = {};
+
+  for (let day = startDate; day <= endDate; day.setDate(day.getDate() + 1)) {
+    const weekNumber = day.getWeekNumber();
+    const weekDay = day.getDay();
+    weeks[weekNumber] = weeks[weekNumber] || {};
+    weeks[weekNumber][weekDay] = new Date(day);
+  }
+  return weeks;
+}
+
+export default Calendar;
